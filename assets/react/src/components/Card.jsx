8969e4aeeeec7e04ebde3fb3e0c307fa608cd567
Fan2Shrek <pierre27.ambroise@gmail.com>
2024-11-10 15:45:03 +0100

    Add node container

--- assets/react/src/components/Card.jsx
+++ assets/react/src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function ({ card, img }) {
+export default function ({ img }) {
     return <div className='card'>
         <img src={ img } />
     </div>;
