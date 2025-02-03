e4f7350d69f43b5ea252944829c4e1f60b5215b3
CharlesLLM <charlesllm@orange.fr>
2025-02-03 18:17:52 +0100

    Move FloatingBackground to component
    Signed-off-by: CharlesLLM <charlesllm@orange.fr>

---
+++ src/Twig/Components/FloatingBackground.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent(template: 'components/ui/FloatingBackground.html.twig')]
+class FloatingBackground
+{
+}
