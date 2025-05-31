06eb4340ef5ddf217de0df476bd5260ad2e97b8f
Pierre Ambroise <pierre27.ambroise@gmail.com>
2025-05-31 21:54:12 +0200

    Add img field to game mode description controller

--- src/Controller/Admin/GameModeDescriptionCrudController.php
+++ src/Controller/Admin/GameModeDescriptionCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\GameModeDescription;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 
 class GameModeDescriptionCrudController extends AbstractCrudController
@@ -19,6 +20,11 @@ class GameModeDescriptionCrudController extends AbstractCrudController
         return [
             AssociationField::new('gameMode'),
             TextEditorField::new('description'),
+            ImageField::new('img')
+                ->setBasePath('/uploads/game_mode')
+                ->setUploadDir('public/uploads/game_mode')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setRequired(false),
         ];
     }
 }
