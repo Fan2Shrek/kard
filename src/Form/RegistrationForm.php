806d66d2f10e7722c412e1fd73319e1abeaf1903
Pierre Ambroise <pierre27.ambroise@gmail.com>
2025-06-20 11:07:34 +0200

    Remove terms from registration and display errors

--- src/Form/RegistrationForm.php
+++ src/Form/RegistrationForm.php
@@ -4,11 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -18,15 +16,6 @@ class RegistrationForm extends AbstractType
     {
         $builder
             ->add('username')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue(
-                        message: 'Vous devez accepter nos conditions.',
-                    ),
-                ],
-                'label' => 'J’accepte les conditions générales',
-            ])
             ->add('plainPassword', PasswordType::class, [
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
