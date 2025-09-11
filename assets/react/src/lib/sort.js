eef8c81b02e30a6f66c7d81124bd76f424f68acf
Pierre Ambroise <pierre27.ambroise@gmail.com>
2025-09-11 11:37:40 +0200

    Fix prod react build

--- assets/react/src/lib/sort.js
+++ assets/react/src/lib/sort.js
@@ -1,2 +1,4 @@
 export const sortByRank = (cards, rankOrder) => cards.sort((a, b) => rankOrder.indexOf(a.rank) - rankOrder.indexOf(b.rank));
 export const sortBySuit = (cards) => cards.sort((a, b) => a.suit.localeCompare(b.suit));
+
+export default { sortByRank, sortBySuit };
