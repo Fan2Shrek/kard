aaac93f73b173c9d42a27681c5aee5001528d1d2
Pierre Ambroise <pierre27.ambroise@gmail.com>
2025-09-26 15:00:50 +0200

    Move devops stuf

--- .devops/docker/build-env.sh
+++
@@ -1,9 +0,0 @@
-rm -f .env.local
-touch .env.local
-
-echo "APP_ENV=$(echo "${APP_ENV:-dev}")" >> .env.local
-
-echo "MERCURE_PUBLIC_URL=$(echo "${MERCURE_PUBLIC_URL:-http://localhost:3000/.well-known/mercure}")" >> .env.local
-echo "MERCURE_JWT_SECRET=$(echo "${MERCURE_JWT_SECRET:-!ChangeThisMercureHubJWTSecretKey!}")" >> .env.local
-
-echo "DATABASE_URL=$(echo "${DATABASE_URL:-mysql://root:root@mysql:3306/symfony}")" >> .env.local
